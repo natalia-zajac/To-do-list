f6cba7680168544d3b4c127045f977b65a8637e8
Natalia Zajac <natalia.zajac@hotmail.com>
2020-07-02 17:13:51 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -12,20 +12,6 @@
     ];
 
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-        <li ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
-            ${task.content}
-        </li>
-        `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
@@ -33,6 +19,36 @@
 
         render();
     };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+        <li ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+        <button class="js-remove">usuń</button>
+            ${task.content}
+        </li>
+        `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+        
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+              removeTask(index);
+            });
+        });
+    };
+
+
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -42,10 +58,10 @@
         if (newTaskContent === "") {
             return;
         }
-        
+
 addNewTask(newTaskContent);
        
-    });
+    };
 
     const init = () => {
         render();
